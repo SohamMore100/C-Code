23ced0dbbaa5cf9894b5306c95015839ec46059a
Soham More <141304014+SohamMore100@users.noreply.github.com>
2024-07-15 18:30:42 +0530

    2D Array Code

---
+++ 2D_Array.cpp
@@ -0,0 +1,11 @@
+// 2D Array Code
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[2][3] = {{100, 200, 300},
+                     {6, 5, 4}};
+    cout << arr[0][2] << endl;
+}
