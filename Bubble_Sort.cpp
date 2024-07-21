0a55c068fddeb007d3800a0004e167c71abcb2e8
Soham More <141304014+SohamMore100@users.noreply.github.com>
2024-07-21 10:47:03 +0530

    Bubble Sort Code

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,30 @@
+//  Bubble Sort Code
+#include <iostream>
+using namespace std;
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    print(arr, n);
+}
+int main()
+{
+    int arr[6] = {5, 4, 6, 3, 1, 2};
+    bubbleSort(arr, 6);
+    return 0;
+}
